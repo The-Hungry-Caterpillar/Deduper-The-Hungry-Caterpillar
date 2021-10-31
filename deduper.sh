7c0c598f4ef60524d3217c467569cb9ff178dde1
The-Hungry-Caterpiillar <ogata1991@gmail.com>
2021-10-31 16:05:07 -0700

    created deduper shell script. WIP

---
+++ deduper.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#SBATCH --partition=bgmp
+#SBATCH --account=bgmp
+#SBATCH --job-name=deduper
+#SBATCH --output=deduper_%j.out
+#SBATCH --error=deduper_%j.out
+#SBATCH --time=1-00:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+
+# help functions, displays user options
+help()
+{
+    echo "This program takes single-end sorted or unsorted sam files and removes PCR duplicates"
+    echo " "
+    echo "The following options are required:"
+    echo "     -f     Input samfile name."
+    echo " "
+    echo "The following options are optional:"
+    echo "     -s     Input any value if the input samfile is not already sorted. YOU MUST SORT THE SAMFILE IF NOT ALREADY SORTED!"
+    echo " "
+}
+
+# process input options
+while getopts ":hf:s:" option
+do
+    case $option in 
+        
+        h) #displays help
+            help
+            exit;;
+
+        f) #enter a file name
+            file=$OPTARG;;
+
+        s) #enter if samtools needs to sort or not
+            sort=$OPTARG;;
+
+        \?) #displays invalid option
+            echo "Error: Invalid option(s)"
+            exit;;
+
+    esac
+done
+
+#checks if -f was passed with a file, if not then exits script.
+if ! [ -f "$file" ]; then
+    echo "Error: You must pass a valid file to the -f option; see -h for help."
+    exit 1
+fi
+
+
+if [ -n $sort ]; then
+    module load samtools/1.5
+    samtools view -S -b $file > inter.bam
+    rm $file
+    samtools sort inter.bam -o sorted.bam
+    samtools view sorted.bam > $file
+fi
+
